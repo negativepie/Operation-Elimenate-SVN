435a7f451edcd7fd7b4359c57f0b3a4104e526fc
Kate98 <31945554+Kate98@users.noreply.github.com>
2017-10-11 22:35:20 +1030

    Add files via upload

---
+++ Testing.cpp
@@ -0,0 +1,57 @@
+/* test cases
+Test 1 - test for invalid attack options
+Test 2 - test for change of variables
+Test 3 - test for 'death'
+Test 4 - test for randomness of enemy attacks */
+
+//Test 1
+int main(){
+
+game newgame = game();
+
+player user = player("Harry", 50,40,20,40);
+
+enemy enemy1 = enemy();
+
+enemy1.changename("Monster");
+
+user.changename("Larry");
+
+std::cout<< enemy1.getname() << user.getname() << std::endl;
+
+}
+
+//Test 2
+int main(){
+
+game newgame = game();
+
+player user = player("Harry", 50,40,20,40);
+
+enemy enemy1 = enemy();
+
+player.changeatkstat(30);
+player.changespdstat(20);
+player.changedefstat(50);
+
+}
+
+//Test 3
+
+int main(){
+game newgame = game();
+
+player user = player("Harry", 50,40,20,40);
+
+enemy enemy1 = enemy();
+enemy enemy2 = enemy();
+enemy enemy3 = enemy();
+enemy1.changename("MonsterA");
+enemy2.changename("MonsterB");
+enemy3.changename("MonsterC");
+
+battle firstbattle(player user, enemy enemy1, enemy enemy2, enemy enemy3);
+
+//this is too check that the enemies die properly and then change of enemy
+
+}
