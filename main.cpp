439a0a88bbd36dc607233352fb728f8fbdaafa45
Kate98 <31945554+Kate98@users.noreply.github.com>
2017-10-13 12:32:38 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,15 @@ int main(){
 	game newgame = game();
 	newgame.gameIntro();
 	game* gameptr=&newgame;
-
+	
+	std::cout << "Please enter character name..." << std::endl;
+	
+	string your_name;
+	
+	std::cin >> your_name;
+	
 	//e x t e n s i v e t e s t i n g
-	player reimu = player("Reimu Hakurei",69,420,8008135,15);
+	player user = player(your_name,69,420,8008135,15);
 
 	enemy enemy1 = enemy();
 	enemy enemy2 = enemy();
@@ -25,7 +31,7 @@ int main(){
 	//player * reimuptr = &reimu;
 	//enemy * enemyptr = &basicenemy;
 
-	battle firstbattle = battle(reimu,enemy1,enemy2,enemy3,gameptr);
+	battle firstbattle = battle(user,enemy1,enemy2,enemy3,gameptr);
 	//std::cout << reimu.getname() << " you are now in a battle with a " << basicenemy.getname() << std::endl;
 	//firstbattle.chooseatk();
 
